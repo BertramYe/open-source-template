0f8b557fd239966c02b9a74b3238488d4b007739
BertramYe <2516559898@qq.com>
2024-05-09 17:24:58 +0800

    try to remove the base settinbg

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,10 @@
 
-import { Link } from "react-router-dom"
+import { 
+    Link,
+    // Outlet 
+
+} from "react-router-dom"
+
 
 
 const  HomePage = ( ) => {
@@ -7,6 +12,8 @@ const  HomePage = ( ) => {
         <>
         <div> this is a project to show my own opensource template </div>
         <div> click to visit the <Link to={"/mover-resizer-rotater"}  className={"mover-resizer-rotater"} >mover-resizer-rotater</Link> </div>
+        
+        {/* <Outlet/> */}
         </>
        
     )
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,20 +1,32 @@
 import { 
-    // createBrowserRouter,
-    createHashRouter 
+    createBrowserRouter,
+    // createHashRouter,
+      
 } from "react-router-dom";
 import HomePage from "@/pages/Home";
 import MoverResizerRotaterPage from "@/pages/MoverResizerRotater";
 import PageHeader from "@/components/PageHeader";
 
+import { Suspense} from 'react'; // 延迟加载缓冲
 
-const router = createHashRouter([
+
+const router = createBrowserRouter([
     {
-        path:'/open-source-template/',
-        element: <PageHeader title="Home Page"> <HomePage />  </PageHeader>   , 
+        path:'/open-source-template',
+        element: (
+            <Suspense  fallback={<div>Loading...</div>} >   
+                <PageHeader title="Home Page"> <HomePage />  </PageHeader> 
+            </Suspense>
+        ),
+        
     },
     {
         path:'/mover-resizer-rotater',
-        element:  <PageHeader title="mover-resizer-rotater">  <MoverResizerRotaterPage/> </PageHeader>        
+        element:  (
+            <Suspense  fallback={<div>Loading...</div>} >   
+            <PageHeader title="mover-resizer-rotater">  <MoverResizerRotaterPage/> </PageHeader>
+            </Suspense>
+        )
     }
 
 ])
