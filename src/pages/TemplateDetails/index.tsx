58ac8f7b6c40e506b2f2e21c08730a5f050612fa
BertramYe <2516559898@qq.com>
2025-01-18 09:41:41 +0800

    upgrade the React version into 19

--- src/pages/TemplateDetails/index.tsx
+++ src/pages/TemplateDetails/index.tsx
@@ -6,11 +6,11 @@ import MoverResizerRotaterPage from "@/pages/MoverResizerRotater";
 import NotFoundPage from "@/pages/NotFound";
 import TemplateHomePage from  '@/pages/TemplateHome'
 import ThreeJS3DPage from "../3DThreeJS";
-
+import { ReactElement } from "react";
 
 const TemplateDetailsPages = () => {
 
-    function GetTargetValidTemplate( input_template_name:string | null | undefined ) :[string, JSX.Element]
+    function GetTargetValidTemplate( input_template_name:string | null | undefined ) :[string, ReactElement]
     {
         const [AvaliableTemplateNamesList,] =  useState({
             'mover-resizer-rotater':<MoverResizerRotaterPage />,
